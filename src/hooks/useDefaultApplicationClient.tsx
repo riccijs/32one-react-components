f77e37b151b2fdc556a89c29a8840f41374d0e2f
Bryan Ricci <bryan.ricci@me.com>
2020-10-31 23:14:56 -0400

    T

--- src/hooks/useDefaultApplicationClient.tsx
+++ src/hooks/useDefaultApplicationClient.tsx
@@ -3,11 +3,11 @@ import Axios from 'axios'
 
 export default function useDefaultApplicationClient() {
   const getClientName = useCallback(async () => {
-    return await Axios.get('https://api-dev.32one.live/v1//client/name')
+    return await Axios.get('https://api-dev.32one.live/v1/client/name')
   }, [])
 
   const setClientId = useCallback(async (clientName: string) => {
-    return await Axios.get(`https://api-dev.32one.live/v1//client/id/${clientName}`)
+    return await Axios.get(`https://api-dev.32one.live/v1/client/id/${clientName}`)
   }, [])
 
   return {
--- src/hooks/useDefaultApplicationUser.tsx
+++ src/hooks/useDefaultApplicationUser.tsx
@@ -11,27 +11,27 @@ export default function useDefaultApplicationUser() {
   const checkIfUsernameOrEmailAddressExists = async (usernameOrEmail: string, isGlobalCheck?: boolean) => {
     const isEmailAddress = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,12})+$/.test(usernameOrEmail)
     const data = isEmailAddress ? { emailAddress: usernameOrEmail } : { username: usernameOrEmail }
-    return await Axios.post('https://api-dev.32one.live/v1//user/exists', { ...data, isGlobalCheck })
+    return await Axios.post('https://api-dev.32one.live/v1/user/exists', { ...data, isGlobalCheck })
   }
 
   const signIn = useCallback(async (password: string) => {
-    return await Axios.post('https://api-dev.32one.live/v1//user/sign-in', { password })
+    return await Axios.post('https://api-dev.32one.live/v1/user/sign-in', { password })
   }, [])
 
   const sendEmail = useCallback(async (options: SendEmailOptions) => {
-    return await Axios.post('https://api-dev.32one.live/v1//client/email-delivery/send', options)
+    return await Axios.post('https://api-dev.32one.live/v1/client/email-delivery/send', options)
   }, [])
 
   const verifyContactInformation = useCallback(async (searchStr: string) => {
-    return await Axios.post(`https://api-dev.32one.live/v1//user/contact-information/verify${searchStr}`)
+    return await Axios.post(`https://api-dev.32one.live/v1/user/contact-information/verify${searchStr}`)
   }, [])
 
   const resetPassword = useCallback(async (searchStr: string, password: string) => {
-    return await Axios.post(`https://api-dev.32one.live/v1//user/reset-password${searchStr}`, { password })
+    return await Axios.post(`https://api-dev.32one.live/v1/user/reset-password${searchStr}`, { password })
   }, [])
 
   const checkPreviousRegistration = useCallback(async (emailAddress: string) => {
-    return await Axios.get(`https://api-dev.32one.live/v1//user/check-registration/${emailAddress}`)
+    return await Axios.get(`https://api-dev.32one.live/v1/user/check-registration/${emailAddress}`)
   }, [])
 
   const checkUsernameAvailability = useCallback(async (username) => {
@@ -39,11 +39,11 @@ export default function useDefaultApplicationUser() {
   }, [])
 
   const register = useCallback(async (user: SignUpUser) => {
-    return await Axios.post('https://api-dev.32one.live/v1//user/add', user)
+    return await Axios.post('https://api-dev.32one.live/v1/user/add', user)
   }, [])
 
   const registerClient = useCallback(async () => {
-    return await Axios.post('https://api-dev.32one.live/v1//user/add-client')
+    return await Axios.post('https://api-dev.32one.live/v1/user/add-client')
   }, [])
 
   return {
